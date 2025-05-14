7bd57a3877f927172fcd5a4754b8fbecef48c35e
codename776 <eminburkhanov@gmail.com>
2025-05-14 11:31:09 -0400

    Update SignedText.java

--- Q2/SignedText.java
+++ Q2/SignedText.java
@@ -1,1 +1,26 @@
+public class SignedText {
+    private String firstName;
+    private lastName;
 
+    public SignedText(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+
+        public String getSignature() {
+            if(firstName.length == 0) {
+                return lastName;
+
+            }
+            return firstName.charAt(0) + "-" + lastName;
+        }
+        public String addSignature(){
+            String s = getSignature();
+            String sLength = signature.length();
+            String msLength = message.length();
+
+            if (mLength >= sLength && message.substring(mLength - sLength).equals(signature)){
+                return message + signature;
+            }
+        }
+    }
+}
