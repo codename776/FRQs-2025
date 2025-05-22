6b1d9899dc0e871d0f68cf10a6f0c0cb98cb7d8e
codename776 <eminburkhanov@gmail.com>
2025-05-22 11:52:29 -0400

    Update Puzzle.java

--- Q4/Puzzle.java
+++ Q4/Puzzle.java
@@ -1,1 +1,21 @@
+// FRQ 4A
 
+// initialize a 2d array with random ints 1-9
+
+// the name of the constructor didn't correlate with the code at all so I don't remember it
+
+private int[][] matrix;
+
+public SumofPre(int numRows, int numCols) {
+
+matrix = new int\[numRows\]\[numCols\]; 
+
+for (int i = 0; i < numRows; i++) { 
+
+	for (int j = 0; j < numCols; j++) { 
+
+		matrix\[i\]\[j\] = ((int)  (Math.random() \* 9) + 1);
+
+	}
+
+}
